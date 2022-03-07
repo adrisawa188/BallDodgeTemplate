9c5f6a99d41cb1bf2dbc6f79b3a7171f3ae09c48
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2022-03-07 10:03:28 -0500

    Added balls. Worked on player movement

--- BallDodgeTemplate/GameScreen.cs
+++ BallDodgeTemplate/GameScreen.cs
@@ -13,10 +13,18 @@ namespace BallDodgeTemplate
     public partial class GameScreen : UserControl
     {
         Ball chaseBall;
+        Player hero;
+
+        List<Ball> dodgeBalls = new List<Ball>(); 
 
         Random randGen = new Random();
         public static int gsWidth = 600;
         public static int gsHeight = 600;
+
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
 
         public GameScreen()
             
@@ -28,6 +36,15 @@ namespace BallDodgeTemplate
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             chaseBall.Move();
+            foreach (Ball b in dodgeBalls)
+            {
+                b.Move();
+            }
+
+            if (leftArrowDown == true)
+            {
+                
+            }
 
             Refresh();
         }
@@ -36,12 +53,28 @@ namespace BallDodgeTemplate
         {
             int x = randGen.Next(40, gsWidth - 40);
             int y = randGen.Next(40, gsHeight - 40);
+
             chaseBall = new Ball(x, y, 8, 8);
+
+            x = randGen.Next(40, gsWidth - 40);
+            y = randGen.Next(40, gsHeight - 40);
+            hero = new Player(x, y);
+
+            for (int i = 0; i < 3; i++)
+            {
+               x = randGen.Next(40, gsWidth - 40);
+               y = randGen.Next(40, gsHeight - 40);
+
+                Ball b = new Ball(x, y, 8, 8);
+                dodgeBalls.Add(b);  
+            }
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
-
+            //switch 
+                //case Keys.Left:
+                
         }
 
         private void GameScreen_KeyUp(object sender, KeyEventArgs e)
@@ -52,6 +85,13 @@ namespace BallDodgeTemplate
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillEllipse(Brushes.Green, chaseBall.x, chaseBall.y, chaseBall.size, chaseBall.size);
+
+            foreach (Ball b in dodgeBalls)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, b.x, b.y, b.size, b.size);
+            }
+
+            e.Graphics.FillRectangle(Brushes.DodgerBlue, hero.x, hero.y, hero.width, hero.height);  
         }
     }
 }
